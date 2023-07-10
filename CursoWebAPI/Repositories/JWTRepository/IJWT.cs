3d9f8cfc2dceb00365c8e4a3874624314d211317
Francisco J. Medina <fjmedina21@outlook.com>
2023-07-10 14:38:30 -0400

    Update IJWT.cs
    Signed-off-by: Francisco J. Medina  <fjmedina21@outlook.com>

--- CursoWebAPI/Repositories/JWTRepository/IJWT.cs
+++ CursoWebAPI/Repositories/JWTRepository/IJWT.cs
@@ -5,6 +5,6 @@ namespace EmploymentExchangeAPI.Repositories
     public interface IJWT
     {
         public Task<string> CreateJWTAsync(User user);
-        //public Task<List<string>> DecodeJWT(string token);
+        public Task<object> DecodeJWT(string token);
     }
 }
